a0503d96c77dfeefc91490799371ec7324ab5247
Jere Janhunen <djeja003@edu.xamk.fi>
2024-01-26 14:19:41 +0200

    ö

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,37 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
 
 exports.sendEmail = functions.https.onRequest((req, res) => {
-  const formData = req.body;
+  cors(req, res, () => {
+    const formData = req.body;
 
-  // Create a transporter
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'jetcher77@gmail.com',
-      pass: '8VptLhdWzpx2hWc!',
-    },
-  });
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'jetcher77@gmail.com',
+        pass: '8VptLhdWzpx2hWc!',
+      },
+    });
 
-  // Define the email content
-  const mailOptions = {
-    from: 'jetcher77@gmail.com',
-    to: 'jere-janhunen@hotmail.com',
-    subject: `New Form Submission - ${formData.subject}`,
-    text: `Name: ${formData.name}\nEmail: ${formData.email}\nMessage: ${formData.message}`,
-  };
+    // Define the email content
+    const mailOptions = {
+        from: 'jetcher77@gmail.com',
+        to: 'jere-janhunen@hotmail.com',
+      subject: `New Form Submission - ${formData.subject}`,
+      text: `Name: ${formData.name}\nEmail: ${formData.email}\nMessage: ${formData.message}`,
+    };
 
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).send(error.toString());
-    }
-    return res.status(200).send('Email sent successfully!');
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(500).send(error.toString());
+      }
+      return res.status(200).send('Email sent successfully!');
+    });
   });
 });
+
+
+
