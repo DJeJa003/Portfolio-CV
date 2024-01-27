de75c545154e6117db01df537f1984ffda891ed9
Jere Janhunen <djeja003@edu.xamk.fi>
2024-01-27 14:18:20 +0200

    1sec

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
             alert('Email sent successfully!');
             setTimeout(() => {
               window.location.href = 'https://janhunenjere.com'
-            }, 1500);
+            }, 1000);
           } else {
             alert('Failed to send email.');
           }
