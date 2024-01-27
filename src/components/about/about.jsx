790cec064aa0ea9b2f1ad5210b1eca0086a4a907
Jere Janhunen <djeja003@edu.xamk.fi>
2024-01-27 12:34:56 +0200

    cv

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -15,7 +15,7 @@ const About = () => {
                         University of Applied Sciences in Mikkeli. My whole life I've been intrigued by everything about computers and the Internet,
                         and how it all works. That is why I'm becoming a software engineer; to find some answers for my questions. 
                         While I may not have the most experience in programming just yet, I am learning and growing each passing day.</p>
-                        <a href="https://drive.google.com/file/d/1NK9DMNt5LkTNoVDWEqcVcxzVGFFN4HW8/view" download="CV_Janhunen" className='btn'>Download CV</a>
+                        <a href="https://drive.google.com/file/d/15M39efZ3x2JqfYyAsqlfwW5QCXyuLq0_/view" download="CV_Janhunen" className='btn'>Download CV</a>
                     </div>
 
                     <div className='about_skills grid'>
