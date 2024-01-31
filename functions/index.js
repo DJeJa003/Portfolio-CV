80b94a03dca93c70d4ffc00642557d0bc562b5c4
Jere Janhunen <djeja003@edu.xamk.fi>
2024-01-31 08:53:41 +0200

    .env created

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 const cors = require('cors')({ origin: true });
+const emaili = process.env.APP_EMAIL;
+const password = process.env.APP_PASSWORD;
+const email2 = process.env.PERSONAL_EMAIL;
+
 
 exports.sendEmail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
@@ -15,8 +19,8 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: 'jetcher77@gmail.com',
-        pass: 'irmzfsddseqopphh',
+        user: emaili,
+        pass: password,
       },
     });
 
@@ -26,8 +30,8 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
       }
 
     const mailOptions = {
-        from: 'jetcher77@gmail.com',
-        to: 'jere-janhunen@hotmail.com',
+        from: emaili,
+        to: email2,
         subject: `New Form Submission - ${subject || 'No Subject'}`,
         text: `Name: ${name || 'No Name'}\n\nEmail: ${email || 'No Email'}\n\nMessage: ${message || 'No Message'}`,
     };
@@ -39,32 +43,14 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
       }
 
       if (formData.email !== null) {
-        // Handle success on the client-side
         alert("Form successfully sent!");
         setTimeout(() => {
-          // Redirect on the client-side
           res.redirect('https://janhunenjere.com');
         }, 50);
       } else {
-        // Handle failure on the client-side
         alert('Please fill out all fields.');
         return res.status(400).send('Failed to send the form, please fill out all fields.');
       }
-    //   if (res.ok && formData.email !== null) {
-    //     res.status(200).send('Form successfully sent.');
-    //   alert("Form successfully sent!");
-    //   setTimeout(() => {
-    //     res.redirect('https://janhunenjere.com');
-    //   }, 50);
-    //   } else {
-    //     alert('Please fill out all fields.')
-    //     return res.status(400).send('Failed to send the form, please fill out all fields.');
-    //   }
-      
-    //   res.status(200).send('Form successfully sent.');
-    //   setTimeout(() => {
-    //     res.redirect('https://janhunenjere.com');
-    //   }, 50);
     });
   });
 });
