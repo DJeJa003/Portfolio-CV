436d52824ead28401d6a24cd822fe397f6ac7be8
Jere Janhunen <djeja003@edu.xamk.fi>
2024-02-14 10:56:26 +0200

    small fix

--- src/components/home/headerSocials.jsx
+++ src/components/home/headerSocials.jsx
@@ -2,14 +2,14 @@ const HeaderSocials = () => {
     return (
         <div className='home_socials'>
             <a href="https://github.com/DJeJa003" className='home_social-link' target="_blank" rel="noreferrer">
-                <i class="fa-brands fa-github"></i>
+                <i className="fa-brands fa-github"></i>
             </a>
 
             <a href="https://www.linkedin.com/in/jere-janhunen-29b9ba222/" className='home_social-link' target="_blank" rel="noreferrer">
-                <i class="fa-brands fa-linkedin"></i>
+                <i className="fa-brands fa-linkedin"></i>
             </a>
             {/* <a href="https://www.instagram.com/jetcher_" className='home_social-link' target="_blank" rel="noreferrer">
-                <i class="fa-brands fa-instagram"></i>
+                <i className="fa-brands fa-instagram"></i>
             </a> */}
         </div>
     )
